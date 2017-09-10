0e171d05ed4be6714893421ed243f62a03b8d918
Oxana Mecheva <okseniya.m@mail.ru>
2017-09-10 21:47:34 +0500

    error  sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp=require('gulp');
 const pug=require('gulp-pug');
 
+//styles
+const sass=require('gulp-sass');
+const rename=require('gulp-rename');
+const sourcemaps=require('gulp-sourcemaps');
+
 const paths={
     root: './build',
     templates: {
         pages: 'src/templates/pages/*.pug',
         src: 'src/templates/**/*.pug',
         dest: 'build/assets/'
+    },
+    styles: {
+        src: 'src/styles/**/*.sass',
+        dest: 'build/assets/styles'
     }
 }
 
@@ -17,4 +26,15 @@ function templates() {
         .pipe(gulp.dest(paths.root));
 }
 
+//sass
+function styles() {
+    return gulp.src('./src/styles/app.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+    }
+
 exports.templates=templates;
+exports.styles=styles;
