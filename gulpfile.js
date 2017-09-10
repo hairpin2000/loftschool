cb639af2125f37a1c7002af5ef5bc79c8331cf84
Oxana Mecheva <okseniya.m@mail.ru>
2017-09-10 19:35:36 +0500

    zakolebalas

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const   gulp=require('gulp');
-const   pug=require('gulp-pug');
+const gulp=require('gulp');
+const pug=require('gulp-pug');
 
 const paths={
-    root:   './build',
-    templates:  {
-        pages:  'src/templates/pages/*.pug',
-        src:    'src/templates/**/*.pug',
-        dest:   'build/assets/'
+    root: './build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug',
+        dest: 'build/assets/'
     }
 }
 
 //pug
-function    templates() {
-    return  gulp.src(paths.templates.pages)
-        .pipe(pug({pretty:  true}))
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({pretty: true}))
         .pipe(gulp.dest(paths.root));
 }
 
