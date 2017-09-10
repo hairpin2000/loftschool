3d6269346d9862555abdf8a4304aada1cfd18771
Oxana Mecheva <okseniya.m@mail.ru>
2017-09-10 22:37:11 +0500

    with browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp=require('gulp');
 const pug=require('gulp-pug');
 const del=require('del');
+const browserSync=require('browser-syns').create();
 
 //styles
 const sass=require('gulp-sass');
@@ -42,11 +43,30 @@ function clean() {
     return del(paths.root);
 }
 
+//отслеживаем изменение исходников, вносим изменения в templates
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+} 
+
+//ff
+function server() {
+    browserSync.init({
+        server: paths.poot
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
 exports.templates=templates;
 exports.styles=styles;
 exports.del=del;
 
 gulp.task('default', gulp.series(
+    gulp.parallel(styles,templates),
+    gulp.parallel(watch,server)
+));
+
+gulp.task('build', gulp.series(
     clean,
     gulp.parallel(styles,templates)
 ));
