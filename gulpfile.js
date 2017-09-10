4fa90a0b3a3437fc387bef574e0defc57624912f
Oxana Mecheva <okseniya.m@mail.ru>
2017-09-10 23:32:26 +0500

    images icons fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp=require('gulp');
 const pug=require('gulp-pug');
 const del=require('del');
-const browserSync=require('browser-syns').create();
+const browserSync=require('browser-sync').create();
 
 //styles
 const sass=require('gulp-sass');
@@ -18,6 +18,18 @@ const paths={
     styles: {
         src: 'src/styles/**/*.sass',
         dest: 'build/assets/styles'
+    },
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
+    },
+    icons: {
+        src: 'src/icons/**/*.*',
+        dest: 'build/assets/icons/'
+    },
+    fonts: {
+        src: 'src/fonts/**/*.*',
+        dest: 'build/assets/fonts/'
     }
 }
 
@@ -47,26 +59,47 @@ function clean() {
 function watch() {
     gulp.watch(paths.styles.src, styles);
     gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.icons.src, icons);
+    gulp.watch(paths.fonts.src, fonts);
 } 
 
-//ff
+//эмуляция виртуального сервера
 function server() {
     browserSync.init({
-        server: paths.poot
+        server: paths.root
     });
     browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe.dest(paths.images.dest);
+}
+
+function icons() {
+    return gulp.src(paths.icons.src)
+        .pipe.dest(paths.icons.dest);
+}
+
+function fonts() {
+    return gulp.src(paths.fonts.src)
+        .pipe.dest(paths.fonts.dest);
 }
 
 exports.templates=templates;
 exports.styles=styles;
 exports.del=del;
+exports.images=images;
+exports.icons=icons;
+exports.fonts=fonts;
 
 gulp.task('default', gulp.series(
-    gulp.parallel(styles,templates),
+    gulp.parallel(styles, templates, images, icons,fonts),
     gulp.parallel(watch,server)
 ));
 
 gulp.task('build', gulp.series(
     clean,
-    gulp.parallel(styles,templates)
+    gulp.parallel(styles, templates,  images, icons,fonts)
 ));
